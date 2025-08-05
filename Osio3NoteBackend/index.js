b2112d13c17a8e0e74ecfc3522b8dbf71f706307
Vili Auvinen <2207441@edu.karelia.fi>
2025-08-05 20:16:17 +0300

    f

--- Osio3NoteBackend/index.js
+++ Osio3NoteBackend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+//const cors = require("cors");
 
 let notes = [
   {
@@ -28,7 +28,7 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(express.static("dist"));
