c1d120387040e3cd14e9dc89392ff84c818b2a40
Vili Auvinen <2207441@edu.karelia.fi>
2025-08-12 18:57:20 +0300

    j

--- 3.1puhbackend/index.js
+++ 3.1puhbackend/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const app = express();
 const Person = require("./models/person");
 
+app.use(express.json()); // JSON muotoisen pyynnön käsittely.
 // Otetaan morgani käyttöön myös POST pyynnöissä
 morgan.token("body", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
@@ -13,7 +14,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(express.static("dist"));
-app.use(express.json()); // JSON muotoisen pyynnön käsittely.
+
 app.use(morgan("tiny"));
 
 // Ei käytössä oleva taulukko
@@ -158,17 +159,18 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-app.use(unknownEndpoint);
-
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
 };
+app.use(unknownEndpoint);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
