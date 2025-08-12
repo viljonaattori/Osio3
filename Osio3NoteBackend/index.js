9f21f8a7c657744df29b9ec331a40c1a9193b3f1
Vili Auvinen <2207441@edu.karelia.fi>
2025-08-12 17:57:04 +0300

    r

--- Osio3NoteBackend/index.js
+++ Osio3NoteBackend/index.js
@@ -38,21 +38,21 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({ error: "content missing" });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+
+    .catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
@@ -93,10 +93,13 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
 };
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
--- README.md
+++ README.md
@@ -10,3 +10,9 @@ https://osio3.onrender.com
 Render deActivoitu
 
 sovelluksen local käynnistys: node --watch index.js
+
+tuotantoversion tekeminen:
+
+1. frontissa: npm run build
+2. dist kopiointi back endiin komennolla:
+   esim : cp -r dist/ "C:/Users/35844/OneDrive - Karelia Ammattikorkeakoulu Oy/FullstackKurssi/Osio3/Osio3/Osio3NoteBackend"
