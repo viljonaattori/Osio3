2a75eb2d0e930d5898b167eef2b672e36b007a77
Vili Auvinen <2207441@edu.karelia.fi>
2025-08-17 14:31:26 +0300

    testejä

--- Osio3NoteBackend/utils/logger.js
+++ Osio3NoteBackend/utils/logger.js
@@ -1,12 +1,19 @@
 const info = (...params) => {
-  console.log(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(...params)
+  }
 }
 
 const error = (...params) => {
-  console.error(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(...params)
+  }
 }
 
-module.exports = { info, error }
+module.exports = {
+  info,
+  error,
+}
 
 // Tämän exportin jälkeen viittaus muodossa:
 // logger.info('message')
