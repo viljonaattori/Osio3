6c9b4336bd97948f0916952840f373549d413658
Vili Auvinen <2207441@edu.karelia.fi>
2025-08-23 09:22:46 +0300

    f

--- Osio3NoteBackend/app.js
+++ Osio3NoteBackend/app.js
@@ -1,34 +1,38 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const notesRouter = require('./controllers/notes')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
-const app = express()
+const app = express();
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+    logger.error("error connection to MongoDB:", error.message);
+  });
+app.use(express.json());
 
-app.use('/api/login', loginRouter)
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(express.static("dist"));
 
-app.use('/api/notes', notesRouter)
-app.use('/api/users', usersRouter)
+app.use("/api/login", loginRouter);
+app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
 
-module.exports = app
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
--- Osio3NoteBackend/utils/middleware.js
+++ Osio3NoteBackend/utils/middleware.js
@@ -13,6 +13,8 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
@@ -21,9 +23,15 @@ const errorHandler = (error, request, response, next) => {
     error.name === 'MongoServerError' &&
     error.message.includes('E11000 duplicate key error')
   ) {
-    return response
-      .status(400)
-      .json({ error: 'expected `username` to be unique' })
+    return response.status(400).json({
+      error: 'expected `username` to be unique',
+    })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired',
+    })
   }
 
   next(error)
